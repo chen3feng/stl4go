9136397dffaa144ceb7c0410ec181e87b705f2d0
CHEN Feng <chen3feng@gmail.com>
2022-07-27 12:44:16 +0800

    adjust Average

--- compute_test.go
+++ compute_test.go
@@ -7,11 +7,6 @@ func TestSumAs(t *testing.T) {
 	expectEq(t, SumAs[int](a), 5050)
 }
 
-func TestAverageAs(t *testing.T) {
-	a := Range[uint8](0, 101)
-	expectEq(t, AverageAs[int](a), 50)
-}
-
 func TestSum(t *testing.T) {
 	a := Range(0, 101)
 	expectEq(t, Sum(a), 5050)
@@ -19,6 +14,21 @@ func TestSum(t *testing.T) {
 
 func TestAverage(t *testing.T) {
 	a := Range(0, 101)
+	expectEq(t, Average(a), 50)
+}
+
+func TestAverage_Float(t *testing.T) {
+	a := Range(0.0, 101.0)
+	expectEq(t, Average(a), 50.0)
+}
+
+func TestAverage_SmallType(t *testing.T) {
+	a := Range[uint8](0, 101)
+	expectEq(t, Average(a), 50)
+}
+
+func TestAverage_UintPtr(t *testing.T) {
+	a := Range[uintptr](0, 101)
 	expectEq(t, Average(a), 50)
 }
 
