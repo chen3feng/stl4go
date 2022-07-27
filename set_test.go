e34907d682b76b4f00fcd0390f23b52955581e8a
CHEN Feng <chen3feng@gmail.com>
2022-07-27 11:53:27 +0800

    change set to value type and add more make test for vector

--- set_test.go
+++ set_test.go
@@ -7,40 +7,61 @@ import (
 )
 
 func Test_Set_Interface(t *testing.T) {
-	_ = Container[int](NewSet[int]())
+	s := make(Set[int])
+	_ = Container[int](&s)
 }
 
-func Test_New(t *testing.T) {
-	s := NewSet[string]()
+func Test_MakeSet(t *testing.T) {
+	s := make(Set[string])
 	expectEq(t, s.Len(), 0)
 	expectEq(t, s.IsEmpty(), true)
 }
 
-func Test_Of(t *testing.T) {
-	s := NewSetOf("hello", "world")
+func Test_MakeSet2(t *testing.T) {
+	s := Set[string]{}
+	expectEq(t, s.Len(), 0)
+	expectEq(t, s.IsEmpty(), true)
+}
+
+func Test_MakeSetOf(t *testing.T) {
+	s := MakeSetOf("hello", "world")
 	expectEq(t, s.Len(), 2)
 }
 
-func Test_IsEmpty(t *testing.T) {
-	s := NewSet[string]()
+func Test_Set_IsEmpty(t *testing.T) {
+	s := make(Set[string])
 	expectEq(t, s.IsEmpty(), true)
 	s.Add("hello")
 	expectEq(t, s.IsEmpty(), false)
 }
 
-func Test_Clean(t *testing.T) {
-	s := NewSetOf("hello", "world")
+func Test_Set_Clean(t *testing.T) {
+	s := MakeSetOf("hello", "world")
 	s.Clean()
 	expectTrue(t, s.IsEmpty())
 }
 
-func Test_String(t *testing.T) {
-	s := NewSetOf("hello", "world")
+func Test_Set_String(t *testing.T) {
+	s := MakeSetOf("hello", "world")
 	expectTrue(t, strings.HasPrefix(fmt.Sprintf("%v", s), "Set[string]"))
 }
 
-func Test_Add(t *testing.T) {
-	s := NewSet[string]()
+func Test_Set_Has(t *testing.T) {
+	s := MakeSetOf("hello", "world")
+	expectTrue(t, s.Has("hello"))
+	expectTrue(t, s.Has("world"))
+	expectFalse(t, s.Has("!"))
+}
+
+func Test_Set_Get(t *testing.T) {
+	s := MakeSetOf("hello", "world")
+	expectTrue(t, s["hello"])
+	expectTrue(t, s["world"])
+	expectFalse(t, s["!"])
+}
+
+func Test_Set_Add(t *testing.T) {
+	s := make(Set[string])
 	s.Add("hello")
 	s.Add("hello")
 	expectEq(t, s.Has("world"), false)
@@ -49,14 +70,14 @@ func Test_Add(t *testing.T) {
 	expectEq(t, s.Len(), 2)
 }
 
-func Test_AddN(t *testing.T) {
-	s := NewSet[string]()
+func Test_Set_AddN(t *testing.T) {
+	s := make(Set[string])
 	s.AddN("hello", "world")
 	expectEq(t, s.Len(), 2)
 }
 
-func Test_Del(t *testing.T) {
-	s := NewSetOf("hello", "world")
+func Test_Set_Del(t *testing.T) {
+	s := MakeSetOf("hello", "world")
 	s.Del("hello")
 	expectEq(t, s.Len(), 1)
 	s.Del("hello")
@@ -65,21 +86,28 @@ func Test_Del(t *testing.T) {
 	expectEq(t, s.Len(), 0)
 }
 
-func Test_DelN(t *testing.T) {
-	s := NewSetOf("hello", "world")
+func Test_Set_DelN(t *testing.T) {
+	s := MakeSetOf("hello", "world")
 	s.DelN("hello", "world")
 	s.Del("world")
 	expectTrue(t, s.IsEmpty())
 }
 
-func Test_Keys(t *testing.T) {
-	s := NewSetOf("hello", "world")
+func Test_Set_Keys(t *testing.T) {
+	s := MakeSetOf("hello", "world")
 	ks := s.Keys()
 	expectEq(t, 2, len(ks))
 }
 
-func Test_ForEach(t *testing.T) {
-	s := NewSetOf("hello", "world")
+func Test_Set_For(t *testing.T) {
+	s := MakeSetOf("hello", "world")
+	for v := range s {
+		expectTrue(t, v == "hello" || v == "world")
+	}
+}
+
+func Test_Set_ForEach(t *testing.T) {
+	s := MakeSetOf("hello", "world")
 	c := 0
 	s.ForEach(func(string) {
 		c++
@@ -87,8 +115,8 @@ func Test_ForEach(t *testing.T) {
 	expectEq(t, c, 2)
 }
 
-func Test_ForEachIf(t *testing.T) {
-	s := NewSetOf("hello", "world")
+func Test_Set_ForEachIf(t *testing.T) {
+	s := MakeSetOf("hello", "world")
 	c := 0
 	s.ForEachIf(func(string) bool {
 		c++
