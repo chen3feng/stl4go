6039ed6187700805f4b7b96d63c75451d9f072c2
CHEN Feng <chen3feng@gmail.com>
2022-08-11 18:04:48 +0800

    Better type switch syntax (#58)

--- skiplist_findnode.go
+++ skiplist_findnode.go
@@ -10,58 +10,45 @@ func (sl *SkipList[K, V]) findNode(key K) *skipListNode[K, V] {
 	}
 	// For knowned Ordered types, use findNodeFast to improve performance.
 	var iface interface{} = key
-	switch iface.(type) {
+	switch v := iface.(type) {
 	case int:
 		tsl := pointerCast[*SkipList[int, V]](sl)
-		v, _ := iface.(int)
 		return pointerCast[*skipListNode[K, V]](findNodeFast(tsl, v))
 	case string:
 		tsl := pointerCast[*SkipList[string, V]](sl)
-		v, _ := iface.(string)
 		return pointerCast[*skipListNode[K, V]](findNodeFast(tsl, v))
 	case int32:
 		tsl := pointerCast[*SkipList[int32, V]](sl)
-		v, _ := iface.(int32)
 		return pointerCast[*skipListNode[K, V]](findNodeFast(tsl, v))
 	case uint32:
 		tsl := pointerCast[*SkipList[uint32, V]](sl)
-		v, _ := iface.(uint32)
 		return pointerCast[*skipListNode[K, V]](findNodeFast(tsl, v))
 	case int64:
 		tsl := pointerCast[*SkipList[int64, V]](sl)
-		v, _ := iface.(int64)
 		return pointerCast[*skipListNode[K, V]](findNodeFast(tsl, v))
 	case uint64:
 		tsl := pointerCast[*SkipList[uint64, V]](sl)
-		v, _ := iface.(uint64)
 		return pointerCast[*skipListNode[K, V]](findNodeFast(tsl, v))
 	case int8:
 		tsl := pointerCast[*SkipList[int8, V]](sl)
-		v, _ := iface.(int8)
 		return pointerCast[*skipListNode[K, V]](findNodeFast(tsl, v))
 	case uint8:
 		tsl := pointerCast[*SkipList[uint8, V]](sl)
-		v, _ := iface.(uint8)
 		return pointerCast[*skipListNode[K, V]](findNodeFast(tsl, v))
 	case int16:
 		tsl := pointerCast[*SkipList[int16, V]](sl)
-		v, _ := iface.(int16)
 		return pointerCast[*skipListNode[K, V]](findNodeFast(tsl, v))
 	case uint16:
 		tsl := pointerCast[*SkipList[uint16, V]](sl)
-		v, _ := iface.(uint16)
 		return pointerCast[*skipListNode[K, V]](findNodeFast(tsl, v))
 	case uintptr:
 		tsl := pointerCast[*SkipList[uintptr, V]](sl)
-		v, _ := iface.(uintptr)
 		return pointerCast[*skipListNode[K, V]](findNodeFast(tsl, v))
 	case float32:
 		tsl := pointerCast[*SkipList[float32, V]](sl)
-		v, _ := iface.(float32)
 		return pointerCast[*skipListNode[K, V]](findNodeFast(tsl, v))
 	case float64:
 		tsl := pointerCast[*SkipList[float64, V]](sl)
-		v, _ := iface.(float64)
 		return pointerCast[*skipListNode[K, V]](findNodeFast(tsl, v))
 	}
 	return sl.findNodeSlow(key)
